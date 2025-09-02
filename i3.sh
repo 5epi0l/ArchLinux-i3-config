e5629632c5f821e963df7cf268fe5f03938b9e7f
5epi0l <91630053+5epi0l@users.noreply.github.com>
2025-09-02 20:24:09 +0530

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -3,16 +3,18 @@
 echo "[+] Installing packages"
 sudo pacman -S polybar feh python-pywal picom rofi dunst ly alacritty xsessions xauth zig pam xorg xorg-xauth brightnessctl nemo
 
-echo "\n[+] cloning the config repo"
+echo "[+] cloning the config repo"
 cd /opt && sudo git clone https://github.com/5epi0l/ArchLinux-i3-config.git
 
-echo "\n[+] Downloading Hack Nerd Font"
+echo "[+] Downloading Hack Nerd Font"
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip" -O ~/Downloads/Hack.zip && sudo unzip ~/Downloads/Hack.zip -d /usr/share/fonts/TTF/
 
-echo "\n[+] Setting up Wallpapers"
+echo "[+] Setting up Wallpapers"
 mkdir ~/Pictures/Wallpapers && wget "https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/mountains.png" -O ~/Pictures/Wallpapers/manga.png
 
-echo "\n[+] Setting up"
+
+
+echo "[+] Setting up"
 cp /opt/ArchLinux-i3-config/config ~/.config/i3/
 mkdir ~/.config/alacritty && cp /opt/ArchLinux-i3-config/alacritty.toml ~/.config/alacritty/
 mkdir ~/.config/picom && cp /opt/ArchLinux-i3-config/picom.conf ~/.config/picom/
@@ -23,7 +25,10 @@ cp /opt/ArchLinux-i3-config/launch.sh ~/.config/polybar/ && chmod +x ~/.config/p
 sudo mkdir /etc/ly && sudo cp /opt/ArchLinux-i3-config/config.ini /etc/ly/
 
 sudo systemctl disable lightdm && sudo systemctl enable ly
+cd /opt &&  git clone --depth=1 https://github.com/adi1090x/rofi.git
+cd rofi && chmod +x setup.sh
+/opt/rofi/setup.sh
 
-echo "\n[+] rebooting"
+echo "[+] rebooting"
 reboot
 
